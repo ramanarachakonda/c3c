20f056eb4eed458e280336fbb7a0b55f523873b1
Ramana Rachakonda <ramana.rachakonda@intel.com>
2020-06-14 17:59:43 -0500

    Update Combined.py
    Added stuff

--- Combined.py
+++ Combined.py
@@ -14,18 +14,31 @@ burl_url = 'https://services1.arcgis.com/X3dmaNvzWUnemDAc/arcgis/rest/services/C
 
 gloc_url = 'https://services5.arcgis.com/ALQeR5k3182nooX1/arcgis/rest/services/COVID19/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&resultOffset=0&resultRecordCount=25&resultType=standard&cacheHint=true'
 
-def get_max_cases(hist_df, city, current):
-    if not city:
-        return (None, None, current, True)
-    try:
-        city_df = hist_df[hist_df['City'] == city]
-        new_current = max(max(city_df['Total_Cases']), current) #if Max(total)>max(confirmed) then "Call"
-        county = city_df['County'].values[0]
-        state = city_df['State'].values[0]
+#def get_max_cases(hist_df, city, current):
+#    if not city:
+#        return (None, None, current, True)
+#    try:
+#        city_df = hist_df[hist_df['City'] == city]
+#        new_current = max(max(city_df['Total_Cases']), current) #if Max(total)>max(confirmed) then "Call"
+#        county = city_df['County'].values[0]
+#        state = city_df['State'].values[0]
+#        return state, county, new_current, not(current != new_current)
+#    except:
+#        return get_max_cases(hist_df, city[:-3], current)
+#
 
-        return state, county, new_current, not(current != new_current)
-    except:
-        return get_max_cases(hist_df, city[:-3], current)
+def get_max_cases(hist_df, city, current):
+    city_df = hist_df[hist_df['City'] == city]
+    if not city_df.empty:
+        prev_max = max(city_df['Total_Cases'])
+        county = city_df['COUNTY'].values[0]
+        state = city_df['State'].values[0]
+        if (current < prev_max): print("*** SOMETHING IS WRONG", city, 'current=',current,'PrevMax=',prev_max)
+        return state, county, current, not(current < prev_max)
+    else:
+        print("*** CITY NOT FOUND ", city, 'current=',current)
+        return None, city, current, True
+        
 
 
 def main():
@@ -47,6 +60,7 @@ def main():
 
         for city in parsed_json['features']:
             cityname = (city['attributes'][name]).upper()
+            if cityname == 'WASHINGTON TWP': continue
             cur_data = int(city['attributes'][cases])
             state, county, new_current, changed = get_max_cases(hist_df, cityname, cur_data)
             if has_date:
@@ -71,7 +85,7 @@ def main():
     for m in re.findall(r';([A-Z\s*]+\s*(TOWNSHIP|BOROUGH|CITY|HI-NELLA|TAVISTOCK))\s*<br>\s*([0-9]+);([0-9]+)', html_content, re.MULTILINE | re.DOTALL):
         city, total_cases = m[0].upper(), m[2]
         
-        state, county, new_current, changed = get_max_cases(hist_df, city, total_cases)
+        state, county, new_current, changed = get_max_cases(hist_df, city, int(total_cases))
         if not state:
             state = 'New Jersey'
             county = 'Camden'
